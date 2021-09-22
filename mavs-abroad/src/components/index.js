9e35e59e7992ebc729dfc8a60c83f112920dfe0d
Rachit Agarwal <rachitrajeshagarwal8484@gmail.com>
2021-09-22 11:23:06 -0500

    dom structure fix

--- mavs-abroad/src/components/index.js
+++ mavs-abroad/src/components/index.js
@@ -3,24 +3,3 @@ export { default as Footer } from "./Footer";
 export { default as Home } from "./Home";
 export { default as About } from "./About";
 export { default as Contact } from "./Contact";
-
-import React from "react";
-import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About, Contact } from "./components";
-function App() {
-  return (
-    <div className="App">
-      <Router>
-        <Navigation />
-        <Switch>
-          <Route path="/" exact component={() => <Home />} />
-          <Route path="/about" exact component={() => <About />} />
-          <Route path="/contact" exact component={() => <Contact />} />
-        </Switch>
-        <Footer />
-      </Router>
-    </div>
-  );
-}
-
-export default App;
